849e7aa620ecd8d3029e315504d9c19231849750
isaiasleite <isaiasleite230@gmail.com>
2024-06-15 13:41:28 -0400

    feat: adição das configurações do prisma

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { UserModule } from './user/user.module';
+import { ExpenseModule } from './expense/expense.module';
 
 @Module({
-  imports: [],
+  imports: [UserModule, ExpenseModule],
   controllers: [AppController],
   providers: [AppService],
 })
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,22 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const options = new DocumentBuilder()
+    .setTitle('API Documentation')
+    .setDescription('API documentation for your NestJS application')
+    .setVersion('1.0')
+    .addTag('API')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, options);
+  SwaggerModule.setup('/swagger/api', app, document);
+
+  app.enableCors();
+
   await app.listen(3000);
 }
 bootstrap();
